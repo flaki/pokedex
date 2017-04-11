713afea0a67e63e9070f5c78b58e073099ad6ade
Flaki <git@flaki.hu>
2017-04-11 13:50:45 +0200

    Networked API version using pokeapi.co

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
-var poke = [
-{
-  "id": 1,
-  "name": "Bulbasaur",
-  "type": [ "grass", "poison" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-},
-{
-  "id": 5,
-  "name": "Charmeleon",
-  "type": [ "fire" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
-},
-{
-  "id": 15,
-  "name": "Beedrill",
-  "type": [ "bug", "poison" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png"
-},
-{
-  "id": 193,
-  "name": "Yanma",
-  "type": [ "bug", "flying" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/193.png"
-},
-{
-  "id": 152,
-  "name": "Chikorita",
-  "type": [ "grass" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png"
-},
-];
+// Why //? http/https universal protocol
+var POKE_API_URL = '//pokeapi.co/api/v2/pokemon/?limit=50';
+var POKE_SPRITE_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/###.png';
 
 
-setTimeout(function() {
-  console.log(poke);
-  renderPokemons(poke);
-}, 3000);
+// Network API request using XMLHttpRequest
+function pokeXhr() {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', POKE_API_URL);
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if(xhr.status === 200) {
+        var poke = JSON.parse(xhr.responseText);
+        console.log('XHR result: ', poke);
+
+        renderPokemons(poke.results);
+      } else {
+        console.log('Error fetching API data, HTTP/'+xhr.status);
+      }
+    }
+  };
+
+  xhr.send();
+}
+
+
+// Network API request using XMLHttpRequest
+function pokeFetch() {
+  fetch(
+    POKE_API_URL
+
+  // = .then(function(result) { return result.json() })
+  ).then(
+    r => r.json()
+
+  ).then(
+    poke => {
+      console.log('Fetch result: ', poke);
+      renderPokemons(poke.results);
+    }
+  ).catch(
+    error => console.log('Error fetching API data: ', error)
+  )
+}
+
+
+//pokeXhr();
+pokeFetch();
 
 
 function renderPokemons(poke) {
@@ -44,7 +56,10 @@ function renderPokemons(poke) {
   // With map & innerHTML
   var pokemons = poke.map(function(pokemon) {
     var li = document.createElement('li');
-    li.innerHTML = '<img src="'+pokemon.sprite+'"><label>'+pokemon.name+'</label>';
+    var pokemonId = parseInt( pokemon.url.match(/pokemon\/(\d+)/)[1], 10);
+
+    // Capitalization of pokemon.name is done in CSS, instead of in JS
+    li.innerHTML = '<img src="'+POKE_SPRITE_URL.replace('###',pokemonId)+'"><label>'+pokemon.name+'</label>';
     return li;
   });
 
